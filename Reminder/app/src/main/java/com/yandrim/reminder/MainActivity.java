ad4b60effc1886fedabb1515ffcd7785cf696e74
Shurigg <Shuriggbutusov@mail.ru>
2016-07-04 21:08:03 +0300

    Created MainActivity class and layout

---
+++ Reminder/app/src/main/java/com/yandrim/reminder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yandrim.reminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Александр on 04.07.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
