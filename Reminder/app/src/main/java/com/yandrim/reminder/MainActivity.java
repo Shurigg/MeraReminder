87526b8d1c1f5245ee01754f2e8cdbd4db86072e
Shurigg <Shuriggbutusov@mail.ru>
2016-07-05 16:35:52 +0300

    The problem with toolbar is created

--- Reminder/app/src/main/java/com/yandrim/reminder/MainActivity.java
+++ Reminder/app/src/main/java/com/yandrim/reminder/MainActivity.java
@@ -2,14 +2,31 @@ package com.yandrim.reminder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-/**
- * Created by Александр on 04.07.2016.
- */
 public class MainActivity extends Activity {
+    
+    private Toolbar toolbar;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
